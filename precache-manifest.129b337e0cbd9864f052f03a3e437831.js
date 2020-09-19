82d59d3e4842d605ba52e6eafe4db0b4eafb5410
jessicamenius <jessicamenius@gmail.com>
2020-09-19 04:09:58 -0500

    Updates

---
+++ precache-manifest.129b337e0cbd9864f052f03a3e437831.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9f9c4cf09e00cd6c3834bbf8b1d863e",
+    "url": "/jm-react-portfolio/index.html"
+  },
+  {
+    "revision": "e0f326e44c6b6544f1c8",
+    "url": "/jm-react-portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "87bb122c4f6911361b8e",
+    "url": "/jm-react-portfolio/static/js/2.042e986e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jm-react-portfolio/static/js/2.042e986e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0f326e44c6b6544f1c8",
+    "url": "/jm-react-portfolio/static/js/main.e7fb2970.chunk.js"
+  },
+  {
+    "revision": "d726f18b35036d2898dd",
+    "url": "/jm-react-portfolio/static/js/runtime-main.5265d555.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/jm-react-portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
